44be407b2403494ee36d65a3d73355a43341904d
monica <mosice@gmail.com>
2024-10-28 12:18:12 +0100

    feat: 2º conditional if

--- src/main/java/org/msc/Conditionals.java
+++ src/main/java/org/msc/Conditionals.java
@@ -2,15 +2,23 @@ package org.msc;
 
 public class Conditionals {
     public static void main(String[] args) {
-        boolean isActive = false;
 
-        //Imprimir "El usuario está activo" si está activo
-        //Imprimir "El usuario no está activo" si no está activo
+        // crear una variable que sea la edad de un usuario
+        byte age = 51;
 
-        if (isActive){
-            System.out.println("El usuario está activo");
+        // Imprimir elusuario esmayor de edad y tiene 18 o más años
+        if (age >= 18){
+            System.out.println("El usuario es mayor de edad");
         } else {
-            System.out.println("El usuario no está activo");
+            System.out.println("El usuario no es mayor de edad");
         }
+
+
+        //boolean isActive = false;
+        //if (isActive){
+        //    System.out.println("El usuario está activo");
+        //} else {
+        //    System.out.println("El usuario no está activo");
+        //}
     }
 }
