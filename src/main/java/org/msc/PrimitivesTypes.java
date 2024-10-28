0ff0d7824563c2eae58d93c806ecb06d8b90a5ed
monica <mosice@gmail.com>
2024-10-28 11:44:49 +0100

    feat: ex basic stream

---
+++ src/main/java/org/msc/PrimitivesTypes.java
@@ -0,0 +1,13 @@
+package org.msc;
+
+    public class PrimitivesTypes {
+
+    public static void main(String[] args) {
+
+        boolean isActive = false;
+
+        System.out.println(isActive);
+    }
+
+
+}
---
+++ src/main/java/org/msc/Strings.java
@@ -0,0 +1,15 @@
+package org.msc;
+
+public class Strings {
+    public static void main(String[] args) {
+
+        String name = "Mònica";
+
+        String surname = new String("Simó");
+
+        String fullname = name+ " " + surname;
+
+        System.out.println(fullname);
+
+    }
+}
